18184b095dbb3912aab8345161949dd7d64a907c
The future is private. <contact@thefutureisprivate.dev>
2026-08-07 18:22:54 +0200

    Handle Fedora ptrace policy removal

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -201,7 +201,9 @@ require_fixed "kernel.core_pattern = |/bin/false" mkosi.extra/usr/lib/sysctl.d/7
 require_fixed "kernel.oops_limit = 100" mkosi.extra/usr/lib/sysctl.d/70-particleos-hardening.conf
 require_fixed "kernel.warn_limit = 100" mkosi.extra/usr/lib/sysctl.d/70-particleos-hardening.conf
 require_fixed "kernel.printk = 3 3 3 3" mkosi.extra/usr/lib/sysctl.d/70-particleos-hardening.conf
-require_fixed "setsebool -P deny_ptrace=on container_allow_ptrace=off" mkosi.postinst.chroot
+require_fixed "setsebool -P deny_ptrace=on" mkosi.postinst.chroot
+require_fixed "if getsebool container_allow_ptrace" mkosi.postinst.chroot
+require_fixed "setsebool -P container_allow_ptrace=off" mkosi.postinst.chroot
 require_fixed "semodule -X 300 -i" mkosi.postinst.chroot
 require_fixed "chmod 0755 /usr/bin/mount /usr/bin/umount" mkosi.postinst.chroot
 require_fixed "libhardened_malloc.so" mkosi.extra/etc/ld.so.preload
