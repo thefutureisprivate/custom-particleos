506e8f64516a416d371b7ac200deb9c297591a9e
The future is private. <contact@thefutureisprivate.dev>
2026-08-16 14:45:12 +0200

    Advance verified Stalwart image

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -256,7 +256,7 @@ require_fixed "package: custom-particleos" .obs/workflows.example.yml
 reject_fixed "package: custom-particleos-webserver" .obs/workflows.example.yml
 
 require_fixed "ImageId=ParticleOS-Stalwart" "$stalwart_service_config"
-require_fixed "ImageVersion=0.16.17.18" "$stalwart_service_config"
+require_fixed "ImageVersion=0.16.17.19" "$stalwart_service_config"
 require_fixed "Format=disk" "$stalwart_service_config"
 require_fixed "Bootable=no" "$stalwart_service_config"
 reject_fixed "ElTorito=" "$stalwart_service_config"
@@ -305,8 +305,8 @@ for metadata_key in \
         AUTOMATIC_UPDATE ROLLBACK_COMPATIBLE_FROM; do
     require_fixed "$metadata_key=" "$stalwart_service_release"
 done
-require_fixed "STALWART_PACKAGE_RELEASE=17" "$stalwart_service_release"
-require_fixed "IMAGE_VERSION=0.16.17.18" "$stalwart_service_release"
+require_fixed "STALWART_PACKAGE_RELEASE=18" "$stalwart_service_release"
+require_fixed "IMAGE_VERSION=0.16.17.19" "$stalwart_service_release"
 require_fixed "UPDATE_KIND=patch" "$stalwart_service_release"
 require_fixed "AUTOMATIC_UPDATE=yes" "$stalwart_service_release"
 require_fixed "ROLLBACK_COMPATIBLE_FROM=0.16.17.14:0.16.17.16" \
